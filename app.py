e19065c6960f75c12aa50e5a2c2acc01560f6873
Yekaterina Skripchenko <katyaskr@yahoo.com>
2019-02-28 15:28:41 -0400

    add fastai

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import requests, os
 from io import BytesIO
 
 # fastai
-#from fastai import *
-#from fastai.vision import *
-#import fastai
+from fastai import *
+from fastai.vision import *
+import fastai
 
 app = Flask(__name__)
 classes = ['sailboat', 'catamaran', 'motorboat']
@@ -25,7 +25,7 @@ def predict():
         url = request.args['url']
         response = requests.get(url)
 
-        #img_class = open_image(BytesIO(response.content))
+        img_class = open_image(BytesIO(response.content))
         #pred_class,pred_idx,outputs = learn.predict(img_class)
 
     return render_template('predict.html', url = url, boat_type = pred_class)
