3f373acb1142127586fba46d117a2af550494bdb
Yekaterina Skripchenko <katyaskr@yahoo.com>
2019-02-28 15:11:11 -0400

    Show image

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
+import aiohttp, asyncio
+import requests, os
+from io import BytesIO
+
+# fastai
+from fastai import *
+from fastai.vision import *
+import fastai
+
 app = Flask(__name__)
+classes = ['sailboat', 'catamaran', 'motorboat']
 
 @app.route('/')
 def hello_world():
-    return 'Hello, test!'
+    return 'Boat Classifier'
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    url = ''
+    pred_class = ''
+
+    if (request.args):
+        url = request.args['url']
+        response = requests.get(url)
+
+        img_class = open_image(BytesIO(response.content))
+        #pred_class,pred_idx,outputs = learn.predict(img_class)
+
+    return render_template('predict.html', url = url, boat_type = pred_class)
+
